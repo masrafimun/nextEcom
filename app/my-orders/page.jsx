d457beba1f140a524e66181d8d1f5071cc7cd9e7
masrafimun <masrafimunna59@gmail.com>
2025-06-16 01:53:26 +0600

    Update page.jsx

--- app/my-orders/page.jsx
+++ app/my-orders/page.jsx
@@ -52,9 +52,9 @@ const MyOrders = () => {
         }
         }
 
-        useEffect(()=>{
-                loadOrderData();
-        },[token])
+          useEffect(()=>{
+                 loadOrderData() 
+        },[token,user])
 
      
     
