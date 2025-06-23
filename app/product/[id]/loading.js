5ca776938d9994ba170c6fc1494b1f3b76a257f3
Mash-the-killer <masrafimunna111@gmail.com>
2025-06-23 16:37:30 +0600

    Update product page and product card

---
+++ app/product/[id]/loading.js
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-[70vh]">
+      <div className="animate-spin rounded-full h-20 w-20 border-4 border-t-orange-300 border-gray-200"></div>
+    </div>
+  );
+}
+
+export default Loading;
